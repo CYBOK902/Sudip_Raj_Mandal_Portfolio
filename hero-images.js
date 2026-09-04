5c0596612d207d342384e4104422b6295fa9f45c
Sudip Raj Mandal <sudiprajmandal@Sudips-MacBook-Pro.local>
2026-09-05 00:06:07 +0545

    Create portfolio site

---
+++ hero-images.js
@@ -0,0 +1,10 @@
+window.heroImages = [
+  'images/hero-1.png',
+  'images/hero-2.jpeg',
+  'images/hero-3.png',
+  'images/hero-4.png',
+  'images/hero-5.png',
+  'images/hero-6.jpeg'
+];
+
+window.heroImageIntervalMs = 3500;
